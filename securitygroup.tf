a2422a006be2e5c045f271ac1078b2fb8ff117c5
salimnextdev <slimandtek@gmail.com>
2026-01-09 10:42:09 +0100

    ssh addition

--- securitygroup.tf
+++ securitygroup.tf
@@ -18,6 +18,14 @@ resource "aws_security_group" "test_sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress = {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+}
+
   egress {
     from_port        = 0
     to_port          = 0
