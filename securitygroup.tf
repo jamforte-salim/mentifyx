59e2199789fde232a7f5146434a2f7dbe0068795
salimnextdev <slimandtek@gmail.com>
2026-01-09 10:56:58 +0100

    ssh equals sign removed

--- securitygroup.tf
+++ securitygroup.tf
@@ -18,13 +18,13 @@ resource "aws_security_group" "test_sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  ingress = {
+  ingress {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-}
+  }
 
   egress {
     from_port        = 0
