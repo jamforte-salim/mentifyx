4f9fcba24722c9b0944ff360fc5cd36d488fcb3e
salimnextdev <slimandtek@gmail.com>
2026-01-12 12:16:05 +0100

    ipv6 readded

--- securitygroup.tf
+++ securitygroup.tf
@@ -23,6 +23,8 @@ resource "aws_security_group" "test_sg" {
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   egress {
     from_port        = 0
